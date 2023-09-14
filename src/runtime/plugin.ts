4670bda6cf363fd3926a7d594ae79b3bd9f0a1d1
Harlan Wilton <harlan@harlanzw.com>
2023-09-14 13:06:48 +0200

    fix: object plugin with `post` priority

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,30 +1,33 @@
 import type { MetaInput } from '@unhead/schema-org'
 import { PluginSchemaOrg } from '@unhead/schema-org'
-import type { NuxtApp } from 'nuxt/app'
 import type { ModuleOptions } from '../module'
 import { defineNuxtPlugin, injectHead, unref, useRouter, useRuntimeConfig, useSiteConfig } from '#imports'
 
-export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
-  const head = injectHead()
-  const config = useRuntimeConfig().public['nuxt-schema-org'] as ModuleOptions
-  const router = useRouter()
-  const currentRoute = router.currentRoute
+export default defineNuxtPlugin({
+  name: 'nuxt-schema-org:init',
+  enforce: 'post',
+  setup(nuxtApp) {
+    const head = injectHead()
+    const config = useRuntimeConfig().public['nuxt-schema-org'] as ModuleOptions
+    const router = useRouter()
+    const currentRoute = router.currentRoute
 
-  const siteConfig = useSiteConfig()
-  head.use(PluginSchemaOrg({
-    minify: config.minify,
-    resolveMeta: async () => {
-      const route = unref(currentRoute)
-      const meta: MetaInput = {
-        host: siteConfig.url,
-        trailingSlash: siteConfig.trailingSlash,
-        inLanguage: siteConfig.defaultLocale,
-        ...siteConfig as MetaInput,
-        ...route.meta,
-        path: route.path,
-      }
-      await nuxtApp.hooks.callHook('schema-org:meta', meta)
-      return meta
-    },
-  }))
+    const siteConfig = useSiteConfig()
+    head.use(PluginSchemaOrg({
+      minify: config.minify,
+      resolveMeta: async () => {
+        const route = unref(currentRoute)
+        const meta: MetaInput = {
+          host: siteConfig.url,
+          trailingSlash: siteConfig.trailingSlash,
+          inLanguage: siteConfig.currentLocale || siteConfig.defaultLocale,
+          ...siteConfig as MetaInput,
+          ...route.meta,
+          path: route.path,
+        }
+        await nuxtApp.hooks.callHook('schema-org:meta', meta)
+        return meta
+      },
+    }))
+  },
 })
