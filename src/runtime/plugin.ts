45d0a61255e36abe1d55a9687710afeae05ee5a9
Harlan Wilton <harlan@harlanzw.com>
2023-10-31 17:08:02 +0900

    fix: Unhead 1.8 compatibility

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,33 +1,33 @@
-import type { MetaInput } from '@unhead/schema-org'
-import { PluginSchemaOrg } from '@unhead/schema-org'
+import { UnheadSchemaOrg } from '@unhead/schema-org'
+import { joinURL } from 'ufo'
 import type { ModuleOptions } from '../module'
-import { defineNuxtPlugin, injectHead, unref, useRouter, useRuntimeConfig, useSiteConfig } from '#imports'
+import { defineNuxtPlugin, injectHead, useHead, useRouter, useRuntimeConfig, useSiteConfig } from '#imports'
 
 export default defineNuxtPlugin({
   name: 'nuxt-schema-org:init',
-  enforce: 'post',
-  setup(nuxtApp) {
+  setup() {
     const head = injectHead()
     const config = useRuntimeConfig().public['nuxt-schema-org'] as ModuleOptions
     const router = useRouter()
-    const currentRoute = router.currentRoute
 
     const siteConfig = useSiteConfig()
-    head.use(PluginSchemaOrg({
-      minify: config.minify,
-      resolveMeta: async () => {
-        const route = unref(currentRoute)
-        const meta: MetaInput = {
-          host: siteConfig.url,
-          trailingSlash: siteConfig.trailingSlash,
+    useHead({
+      templateParams: {
+        schemaOrg: {
+          ...router.currentRoute.value.meta,
+          ...siteConfig as Record<string, string>,
+          url: () => joinURL(siteConfig.url, router.currentRoute.value.path),
+          host: siteConfig.url!,
           inLanguage: siteConfig.currentLocale || siteConfig.defaultLocale,
-          ...siteConfig as MetaInput,
-          ...route.meta,
-          path: route.path,
-        }
-        await nuxtApp.hooks.callHook('schema-org:meta', meta)
-        return meta
+          path: () => router.currentRoute.value.path,
+        },
       },
-    }))
+    })
+    head.use(
+      UnheadSchemaOrg({
+        minify: config.minify,
+        trailingSlash: siteConfig.trailingSlash,
+      }),
+    )
   },
 })
