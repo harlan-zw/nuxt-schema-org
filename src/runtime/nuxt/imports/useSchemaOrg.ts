e085d472564bfc8050de2bd06d27794f68857103
Harlan Wilton <harlan@harlanzw.com>
2024-02-02 17:34:16 +1100

    chore: fix build error

--- src/runtime/nuxt/imports/useSchemaOrg.ts
+++ src/runtime/nuxt/imports/useSchemaOrg.ts
@@ -1,9 +1,10 @@
 import type { useSchemaOrg as _useSchemaOrg } from '@unhead/schema-org/vue'
-import type { ModuleRuntimeConfig } from '../../types'
+import type {ModuleRuntimeConfig, UnheadAugmentation} from '../../types'
 import { useHead, useRuntimeConfig, useServerHead } from '#imports'
+import type {ActiveHeadEntry} from "@unhead/schema";
 
 type Input = Parameters<typeof _useSchemaOrg>[0]
-export function useSchemaOrg(input: Input) {
+export function useSchemaOrg<T extends Input>(input: T): ActiveHeadEntry<UnheadAugmentation<T>> | void {
   const config = (useRuntimeConfig()['nuxt-schema-org'] || useRuntimeConfig().public['nuxt-schema-org']) as ModuleRuntimeConfig
   const script = {
     type: 'application/ld+json',
@@ -13,13 +14,13 @@ export function useSchemaOrg(input: Input) {
   }
   if (import.meta.server) {
     // we don't need to use the direct composable as the plugin is already registered
-    return useServerHead<{ script: { nodes: Input } }>({
+    return useServerHead<UnheadAugmentation<T>>({
       script: [script],
     })
   }
   else {
     if (config?.reactive) {
-      return useHead<{ script: { nodes: Input } }>({
+      return useHead<UnheadAugmentation<T>>({
         script: [script],
       })
     }
--- src/runtime/types.ts
+++ src/runtime/types.ts
@@ -1,3 +1,7 @@
 import type { ModuleOptions } from '../module'
 
 export type ModuleRuntimeConfig = Pick<ModuleOptions, 'scriptAttributes' | 'reactive' | 'minify' | 'identity'> & { version: string }
+
+export interface UnheadAugmentation<T> {
+  script: { nodes: T }
+}
