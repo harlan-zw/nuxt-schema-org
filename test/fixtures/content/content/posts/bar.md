8289e212b80951083c188f1505ddbaed0cd2958f
harlan <harlan@harlanzw.com>
2024-11-24 18:56:19 +1100

    chore: add tests

--- test/fixtures/content/content/posts/bar.md
+++ test/fixtures/content/content/posts/bar.md
@@ -1,6 +1,11 @@
 ---
-sitemap:
-  loc: /blog/posts/bar
-  lastmod: 2021-10-20
+schemaOrg:
+  - type: "BlogPosting"
+    headline: "How to Use Our Product"
+    author:
+      type: "Person"
+      name: "Jane Smith"
+    datePublished: "2023-10-01"
 ---
+
 # bar
---
+++ test/integration/content.test.ts
@@ -0,0 +1,118 @@
+import { fileURLToPath } from 'node:url'
+import { setup } from '@nuxt/test-utils'
+import { describe, expect, it } from 'vitest'
+import { $fetchSchemaOrg } from './utils'
+
+await setup({
+  rootDir: fileURLToPath(new URL('../fixtures/content', import.meta.url)),
+  server: true,
+  browser: false,
+})
+
+describe('content', () => {
+  it('render faq', async () => {
+    const schema = await $fetchSchemaOrg('/question-answer')
+
+    // Snapshot
+    expect(schema).toMatchInlineSnapshot(`
+      {
+        "@context": "https://schema.org",
+        "@graph": [
+          {
+            "@id": "https://nuxtseo.com/#website",
+            "@type": "WebSite",
+            "description": "The quickest and easiest way to build Schema.org graphs for Nuxt.",
+            "name": "nuxt-schema-org",
+            "url": "https://nuxtseo.com",
+          },
+          {
+            "@id": "https://nuxtseo.com/question-answer/#webpage",
+            "@type": [
+              "WebPage",
+              "FaqPage",
+            ],
+            "description": "hello",
+            "isPartOf": {
+              "@id": "https://nuxtseo.com/#website",
+            },
+            "mainEntity": [
+              {
+                "@type": "Question",
+                "acceptedAnswer": {
+                  "@type": "Answer",
+                  "text": "You can return any item within 30 days of purchase.",
+                },
+                "name": "What is your return policy?",
+              },
+              {
+                "@type": "Question",
+                "acceptedAnswer": {
+                  "@type": "Answer",
+                  "text": "Yes, we offer 24/7 technical support.",
+                },
+                "name": "Do you offer technical support?",
+              },
+            ],
+            "name": "FAQ",
+            "potentialAction": [
+              {
+                "@type": "ReadAction",
+                "target": [
+                  "https://nuxtseo.com/question-answer",
+                ],
+              },
+            ],
+            "url": "https://nuxtseo.com/question-answer",
+          },
+        ],
+      }
+    `)
+  })
+
+  it('render blog', async () => {
+    const schema = await $fetchSchemaOrg('/posts/bar')
+
+    // Snapshot
+    expect(schema).toMatchInlineSnapshot(`
+      {
+        "@context": "https://schema.org",
+        "@graph": [
+          {
+            "@id": "https://nuxtseo.com/#website",
+            "@type": "WebSite",
+            "description": "The quickest and easiest way to build Schema.org graphs for Nuxt.",
+            "name": "nuxt-schema-org",
+            "url": "https://nuxtseo.com",
+          },
+          {
+            "@id": "https://nuxtseo.com/posts/bar/#webpage",
+            "@type": "WebPage",
+            "description": "The quickest and easiest way to build Schema.org graphs for Nuxt.",
+            "isPartOf": {
+              "@id": "https://nuxtseo.com/#website",
+            },
+            "name": "bar",
+            "potentialAction": [
+              {
+                "@type": "ReadAction",
+                "target": [
+                  "https://nuxtseo.com/posts/bar",
+                ],
+              },
+            ],
+            "url": "https://nuxtseo.com/posts/bar",
+          },
+          {
+            "@type": "BlogPosting",
+            "author": {
+              "@type": "Person",
+              "name": "Jane Smith",
+            },
+            "datePublished": "2023-10-01",
+            "headline": "How to Use Our Product",
+          },
+        ],
+      }
+    `)
+  })
+})
