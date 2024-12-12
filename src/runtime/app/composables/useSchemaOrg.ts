3d3d9d6bfc7e2f590fb4e72e0731740a24ed957b
harlan <harlan@harlanzw.com>
2024-12-12 20:11:27 +1100

    fix: render schema.org when indexing is disabled
    Fixes #69

--- src/runtime/app/composables/useSchemaOrg.ts
+++ src/runtime/app/composables/useSchemaOrg.ts
@@ -2,7 +2,6 @@ import type { ActiveHeadEntry, DataKeys, ScriptBase, TagUserProperties } from '@
 import type { useSchemaOrg as _useSchemaOrg } from '@unhead/schema-org/vue'
 import type { UnheadAugmentation } from '../../types'
 import { useHead, useServerHead } from '@unhead/vue'
-import { useRequestEvent } from 'nuxt/app'
 import { useSchemaOrgConfig } from '../utils/config'
 
 type Input = Parameters<typeof _useSchemaOrg>[0]
@@ -22,11 +21,6 @@ export function useSchemaOrg<T extends Input>(input: T): ActiveHeadEntry<UnheadA
     })
   }
   if (import.meta.server) {
-    const event = useRequestEvent()
-    // don't bother rendering schema.org if the page is not indexable
-    if (typeof event?.context.robots !== 'undefined' && !event.context.robots?.indexable) {
-      return
-    }
     // we don't need to use the direct composable as the plugin is already registered
     return useServerHead<UnheadAugmentation<T>>({
       script: [script],
