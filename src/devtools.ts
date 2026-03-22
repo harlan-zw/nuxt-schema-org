b4012a7852d4366a74dd6ac49c778b09d2a3c677
Harlan Wilton <harlan@harlanzw.com>
2026-03-23 00:14:52 +1100

    fix: re-export DEVTOOLS_UI_LOCAL_PORT from devtools

--- src/devtools.ts
+++ src/devtools.ts
@@ -3,7 +3,9 @@ import type { Nuxt } from 'nuxt/schema'
 import { useNuxt } from '@nuxt/kit'
 import { setupDevToolsUI as _setupDevToolsUI } from 'nuxtseo-shared/devtools'
 
-export function setupDevToolsUI(_options: unknown, resolve: Resolver['resolve'], nuxt: Nuxt = useNuxt()) {
+export const DEVTOOLS_UI_LOCAL_PORT = 3030
+
+export function setupDevToolsUI(_options: unknown, resolve: Resolver['resolve'], nuxt: Nuxt = useNuxt()): void {
   _setupDevToolsUI({
     route: '/__nuxt-schema-org',
     name: 'nuxt-schema-org',
