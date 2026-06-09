75e08816f2bbd7f99f89f2e2fdd65aa78365a02e
Harlan Wilton <harlan@harlanzw.com>
2026-06-09 12:23:57 +1000

    chore(devtools): crash

--- devtools/composables/rpc.ts
+++ devtools/composables/rpc.ts
@@ -6,15 +6,21 @@ export const hostHead = ref<VueHeadClient>()
 export const schemaOrgGraph = ref<any>(null)
 
 async function fetchGraph() {
+  // hostHead is only available when the embedded host exposes usehead; without
+  // it there is no graph to resolve, so bail rather than calling a non-function.
+  if (typeof hostHead.value?.resolveTags !== 'function') {
+    schemaOrgGraph.value = {}
+    return
+  }
   schemaOrgGraph.value = 'loading'
-  schemaOrgGraph.value = (await hostHead.value!.resolveTags())
+  schemaOrgGraph.value = (await hostHead.value.resolveTags())
     .filter(t => !!t.props['data-nuxt-schema-org'])[0]
     ?.innerHTML || {}
 }
 
 useDevtoolsConnection({
   onConnected(client) {
-    hostHead.value = client.host.nuxt.vueApp._context.provides.usehead
+    hostHead.value = client?.host?.nuxt?.vueApp?._context?.provides?.usehead
     fetchGraph()
     refreshSources()
   },
